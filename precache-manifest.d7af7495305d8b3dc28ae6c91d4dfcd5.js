36e6146fa66b07328fa7a7571dcc97fd06be119a
Cem KIRAY <jamcry@hotmail.com>
2020-01-18 11:55:35 +0300

    Updates

---
+++ precache-manifest.d7af7495305d8b3dc28ae6c91d4dfcd5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1de01bf10bffe6d3f1dedc3f118c791e",
+    "url": "/react-trivia-game/index.html"
+  },
+  {
+    "revision": "f22b6360c3e4d46168b7",
+    "url": "/react-trivia-game/static/css/2.09ae8418.chunk.css"
+  },
+  {
+    "revision": "6ebf5c84b3c3a97ae0ae",
+    "url": "/react-trivia-game/static/css/main.0756f3de.chunk.css"
+  },
+  {
+    "revision": "f22b6360c3e4d46168b7",
+    "url": "/react-trivia-game/static/js/2.cb0d5d73.chunk.js"
+  },
+  {
+    "revision": "247c6a467a30a5ade18365af945c2fd6",
+    "url": "/react-trivia-game/static/js/2.cb0d5d73.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6ebf5c84b3c3a97ae0ae",
+    "url": "/react-trivia-game/static/js/main.b7bef845.chunk.js"
+  },
+  {
+    "revision": "513cd213e6691b379017",
+    "url": "/react-trivia-game/static/js/runtime-main.aeb59b6a.js"
+  }
+]);
